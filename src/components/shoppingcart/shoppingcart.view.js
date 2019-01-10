0be553eba13bf4846377d1c28aa4dd253226a582
Fabricio Augusto <fabricionaweb@gmail.com>
2019-01-10 22:59:09 +0000

    Update total values in shoppingcart view component

--- src/components/shoppingcart/shoppingcart.view.js
+++ src/components/shoppingcart/shoppingcart.view.js
@@ -1,24 +1,48 @@
 (function (root, MODULE_NAME, NAMESPACE) {
   root[NAMESPACE] = root[NAMESPACE] || {};
 
-  let _$shopForm, _$template, _loader;
+  let _$shopForm, _$template, _$resume, _loader;
   const _options = {
     shopForm: '[data-shop-form]',
-    tbody: '[data-shop-tbody]',
-    template: {
+    shopBody: '[data-shop-tbody]',
+    shopItem: {
       selector: '[data-shop-item-template]',
       productName: '[data-item-name]',
       productDescription: '[data-item-description]',
       productQuantity: '[data-item-quantity]',
     },
+    resume: {
+      selector: '[data-shop-resume]',
+      currencyDataset: 'currency',
+      beforeVAT: '[data-shop-resume-before-vat]',
+      afterVAT: '[data-shop-resume-after-vat]',
+      VATRate: '[data-shop-resume-vat-rate]',
+      VAT: '[data-shop-resume-vat]',
+    },
   };
 
   root[NAMESPACE][MODULE_NAME] = function () {
+    const _getShopFormElement = () => {
+      return _$shopForm || document.querySelector(_options.shopForm);
+    };
+
+    const _getTemplateElement = () => {
+      return _$template || document.querySelector(_options.shopItem.selector);
+    };
+
+    const _getResumeElement = () => {
+      return _$resume || document.querySelector(_options.resume.selector);
+    };
+
+    const _getComponentLoader = () => {
+      return _loader || new root[NAMESPACE].componentLoader();
+    };
+
     const _hideShopForm = () => _$shopForm.setAttribute('hidden', true);
     const _displayShopForm = () => _$shopForm.removeAttribute('hidden');
 
     const _buildProductsRowDOM = (product) => {
-      const { productName, productDescription, productQuantity } = _options.template;
+      const { productName, productDescription, productQuantity } = _options.shopItem;
       const $template = document.importNode(_$template.content, true);
 
       $template.firstElementChild.setAttribute('data-product', JSON.stringify(product));
@@ -30,8 +54,8 @@
       return $template;
     };
 
-    const _updateShopFormProductsDOM = (products) => {
-      const $tbody = document.querySelector(_options.tbody);
+    const _updateCartBodyDOM = ({ products }) => {
+      const $tbody = document.querySelector(_options.shopBody);
       const $fragment = $tbody.cloneNode();
 
       products.map(_buildProductsRowDOM).map(($tr) => $fragment.appendChild($tr));
@@ -39,24 +63,32 @@
       $tbody.replaceWith($fragment);
     };
 
+    const _updateResumeDOM = ({ total, VATRate }) => {
+      const { beforeVAT, VAT, afterVAT } = total;
+      const { resume } = _options;
+      const currency = _$resume.dataset[resume.currencyDataset];
+
+      _$resume.querySelector(resume.VATRate).innerText = `${VATRate}%`;
+      _$resume.querySelector(resume.beforeVAT).innerText = `${currency}${beforeVAT.toFixed(2)}`;
+      _$resume.querySelector(resume.VAT).innerText = `${currency}${VAT.toFixed(2)}`;
+      _$resume.querySelector(resume.afterVAT).innerText = `${currency}${afterVAT.toFixed(2)}`;
+    };
+
     return {
       init: function () {
-        const { shopForm, template } = _options;
-
-        _$shopForm = _$shopForm || document.querySelector(shopForm);
-        _$template = _$template || document.querySelector(template.selector);
-
-        _loader = _loader || new root[NAMESPACE].componentLoader();
+        _$shopForm = _getShopFormElement();
+        _$template = _getTemplateElement();
+        _$resume = _getResumeElement();
+        _loader = _getComponentLoader();
       },
 
       refresh: function (cart) {
-        const { products } = cart;
-
-        if (!products.length) {
+        if (!cart.products.length) {
           return _hideShopForm();
         }
 
-        _updateShopFormProductsDOM(products);
+        _updateCartBodyDOM(cart);
+        _updateResumeDOM(cart);
         _displayShopForm();
 
         this.componentsLoader();
