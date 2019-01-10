5ec9270e993a48ede55769c4ddd92cce91cbb7bc
Fabricio Augusto <fabricionaweb@gmail.com>
2019-01-10 23:08:51 +0000

    Change quantity component to also update by input number

--- src/components/shoppingcart/quantity.controller.js
+++ src/components/shoppingcart/quantity.controller.js
@@ -21,7 +21,11 @@
 
         _view.init();
 
-        $element.addEventListener('click', this.onQuantityClick.bind(this));
+        if (_type === 'input') {
+          $element.addEventListener('blur', this.onQuantityBlur.bind(this));
+        } else {
+          $element.addEventListener('click', this.onQuantityClick.bind(this));
+        }
       },
 
       getProduct: function () {
@@ -45,6 +49,13 @@
           newQuantity = _product.quantity - 1;
         }
 
+        const cart = _model.changeProductQuantity(_product, newQuantity);
+
+        _view.refresh(cart);
+      },
+
+      onQuantityBlur: function ({ target }) {
+        const newQuantity = parseInt(target.value);
         const cart = _model.changeProductQuantity(_product, newQuantity);
 
         _view.refresh(cart);
